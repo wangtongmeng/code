4974c0b9546ee24fb3f4b943131ec7665217f45e
wangtongmeng <tongmengwang@163.com>
2021-11-07 10:31:24 +0800

    redux中间件

--- FrontEnd/react/redux/src/7.react-redux中间件/2.中间件级联/index.js
+++ FrontEnd/react/redux/src/7.react-redux中间件/2.中间件级联/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Counter1 from './components/Counter1';
-import Counter3 from './components/Counter3';
+import Counter2 from './components/Counter2';
+// import Counter3 from './components/Counter3';
 import { Provider } from 'react-redux';
 import store from './store';
 ReactDOM.render(
 <Provider store={store}>
     <Counter1 />
-    <Counter3 />
+    <Counter2 />
 </Provider>, document.getElementById('root'));
--- FrontEnd/react/redux/src/7.react-redux中间件/2.中间件级联/redux-thunk.js
+++ FrontEnd/react/redux/src/7.react-redux中间件/2.中间件级联/redux-thunk.js
@@ -1,7 +1,7 @@
 function thunk({getState,dispatch}){
     return function(next){//调用下一个中间件 next=store.原始的dispatch方法
         return function(action){//这个就是改造后的dispatch
-            debugger
+            // debugger
            if(typeof action === 'function'){
               return action(dispatch,getState);
            }
