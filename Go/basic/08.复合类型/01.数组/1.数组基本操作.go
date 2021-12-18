3a86e882a5766518dc7a006559bee3cebf8c7930
wangtongmeng <tongmengwang@163.com>
2021-12-18 08:20:10 +0800

    数组

--- Go/basic/08.复合类型/01.数组/1.数组基本操作.go
+++ Go/basic/08.复合类型/01.数组/1.数组基本操作.go
@@ -3,35 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	/*
-	var Numbers[5] int = [5]int{1,2,3,4,5} // 下标是从0开始计算的。
-	fmt.Println(Numbers[3])
-	*/
-	//  部分赋值
-	/*
-	Numbers := [5]int{1,2}
-	fmt.Println(Numbers[4])
-	*/
+	// 全部初始化
+	var Numbers1 [5]int = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(Numbers1, Numbers1[3]) // [1 2 3 4 5] 4
+	// 部分初始化
+	Numbers2 := [5]int{1, 2}
+	fmt.Println(Numbers2, Numbers2[4]) // [1 2 0 0 0] 0
 	// 指定某个元素初始化
-	/*
-	Numbers :=[5]int{2:5,3:6}
-	fmt.Println(Numbers[3])
-	*/
-	//通过初始化确定数组长度
-	/*
-	Numbers :=[...]int{7,8,5}
-	//fmt.Println(len(Numbers))
-	fmt.Println(Numbers[0])
-	*/
-	var Numbers [5] int
-	/*
-	Numbers[0]=1
-	Numbers[1]=2
-	fmt.Println(Numbers[3])
-	*/
-	for i := 0; i < len(Numbers); i++ {
-		Numbers[i] = i + 1
-	}
-	fmt.Println(Numbers[0])
+	Numbers3 := [5]int{2: 5, 3: 6}
+	fmt.Println(Numbers3, Numbers3[3]) // [0 0 5 6 0] 6
+	// 通过初始化确定数组长度
+	Numbers4 := [...]int{1, 2, 3}
+	fmt.Println(Numbers4, len(Numbers4)) // [1 2 3] 3
 
+	// 通过下标单个赋值
+	var Numbers5 [5]int
+	Numbers5[0] = 1
+	Numbers5[1] = 2
+	fmt.Println(Numbers5) // [1 2 0 0 0]
+	// 通过循环方式赋值
+	for i := 0; i < len(Numbers5); i++ {
+		Numbers5[i] = i + 1
+	}
+	fmt.Println(Numbers5) // [1 2 3 4 5]
 }
--- Go/basic/test.go
+++ Go/basic/test.go
@@ -3,12 +3,27 @@ package main
 import "fmt"
 
 func main() {
-	defer fmt.Println("起床")
-	defer fmt.Println("睡觉")
-	defer fmt.Println("打豆豆")
+	// 全部初始化
+	var Numbers1 [5]int = [5]int{1, 2, 3, 4, 5}
+	fmt.Println(Numbers1, Numbers1[3]) // [1 2 3 4 5] 4
+	// 部分初始化
+	Numbers2 := [5]int{1, 2}
+	fmt.Println(Numbers2, Numbers2[4]) // [1 2 0 0 0] 0
+	// 指定某个元素初始化
+	Numbers3 := [5]int{2: 5, 3: 6}
+	fmt.Println(Numbers3, Numbers3[3]) // [0 0 5 6 0] 6
+	// 通过初始化确定数组长度
+	Numbers4 := [...]int{1, 2, 3}
+	fmt.Println(Numbers4, len(Numbers4)) // [1 2 3] 3
 
+	// 通过下标单个赋值
+	var Numbers5 [5]int
+	Numbers5[0] = 1
+	Numbers5[1] = 2
+	fmt.Println(Numbers5) // [1 2 0 0 0]
+	// 通过循环方式赋值
+	for i := 0; i < len(Numbers5); i++ {
+		Numbers5[i] = i + 1
+	}
+	fmt.Println(Numbers5) // [1 2 3 4 5]
 }
-
-// 打豆豆 多个defer会采用后进先出的栈形式执行
-// 睡觉
-// 起床
