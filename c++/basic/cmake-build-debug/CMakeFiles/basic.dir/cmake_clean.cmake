e55f13217f3039afb1a76324b9872bbbf8064ce0
wangtongmeng <tongmengwang@163.com>
2021-12-27 23:01:51 +0800

    'hello'

---
+++ c++/basic/cmake-build-debug/CMakeFiles/basic.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/basic.dir/main.cpp.obj"
+  "CMakeFiles/basic.dir/ticketmachine.cpp.obj"
+  "basic.exe"
+  "basic.exe.manifest"
+  "basic.pdb"
+  "libbasic.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/basic.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
