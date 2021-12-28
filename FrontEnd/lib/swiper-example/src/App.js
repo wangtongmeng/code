85bfc6bb5eb2cf8557eb29f3e9068c9ad85c4a91
wangtongmeng <tongmengwang@163.com>
2021-12-28 18:50:00 +0800

    swiper

---
+++ FrontEnd/lib/swiper-example/src/App.js
@@ -0,0 +1,27 @@
+
+import { Swiper, SwiperSlide } from 'swiper/react'; 
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
+
+function App() {
+  return (
+    <div className="App">
+      <Swiper
+      spaceBetween={50}
+      slidesPerView={3}
+      onSlideChange={() => console.log('slide change')}
+      onSwiper={(swiper) => console.log(swiper)}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      ...
+    </Swiper>
+    </div>
+  );
+}
+
+export default App;
