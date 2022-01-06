d3ba9fc11311003678b7cd97183078240802b227
wangtongmeng <tongmengwang@163.com>
2022-01-06 23:46:53 +0800

    基本的路由库

--- FrontEnd/react/router-v6/src/index.js
+++ FrontEnd/react/router-v6/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, BrowserRouter, Routes, Route} from 'react-router-dom'
+import {HashRouter, BrowserRouter, Routes, Route} from './react-router-dom'
 import Home from './components/Home'
 import User from './components/User'
 import Profile from './components/Profile'
--- FrontEnd/react/router-v6/src/react-router/index.js
+++ FrontEnd/react/router-v6/src/react-router/index.js
@@ -1,14 +1,68 @@
-function Router(ref){
+import React from "react";
+const NavigationContext = React.createContext();
+const LocationContext = React.createContext();
+const RouteContext = React.createContext();
 
+/**
+ *
+ * @param {*} children 儿子
+ * @param {*} navigator 历史对象，其实就是history
+ * @param {*} location 地址对象，{pathname: "当前路径"}
+ * @returns
+ */
+function Router({ children, navigator, location }) {
+  return (
+    <NavigationContext.Provider value={{ navigator }}>
+      <LocationContext.Provider value={{ location }}>
+        {children}
+      </LocationContext.Provider>
+    </NavigationContext.Provider>
+  );
 }
-function Routes(ref){
-
+function useLocation() {
+  return React.useContext(LocationContext).location;
 }
-function Route(props){
-
+function Routes({ children }) {
+  return useRoutes(createRoutesFromChildren(children));
 }
-export {
-    Router,
-    Routes,
-    Route
+function useRoutes(routes) {
+  let location = useLocation();
+  let pathname = location.pathname || "/";
+  for (let i = 0; i < routes.length; i++) {
+    let { path, element } = routes[i];
+    let match = matchPath(path, pathname);
+    if (match) return element;
+  }
 }
+/**
+ * 判断此route对应的path路径和地址中的pathname是否匹配
+ * @param {*} path
+ * @param {*} pathname
+ */
+function matchPath(path, pathname) {
+  let matcher = compilePath(path);
+  let match = pathname.match(matcher);
+  return match;
+}
+/**
+ * 把灵境转化成正则表达式
+ * @param {*} path 路径
+ */
+function compilePath(path) {
+  let regexpSource = "^" + path + "$";
+  let matcher = new RegExp(regexpSource);
+  return matcher;
+}
+function createRoutesFromChildren(children) {
+  let routes = [];
+  React.Children.forEach(children, (child) => {
+    let route = {
+      path: child.props.path, // 代表Route的路径
+      element: child.props.element, // 代表此Route要渲染的元素
+    };
+    routes.push(route);
+  });
+  return routes;
+}
+function Route(props) {}
+export { Router, Routes, Route };
