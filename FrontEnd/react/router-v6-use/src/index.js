ec62716524f1e7da26e5ce260aa53d98a78ac57c
wangtongmeng <tongmengwang@163.com>
2022-01-13 19:31:22 +0800

    router-v6-use: 获取路由参数

--- FrontEnd/react/router-v6-use/src/index.js
+++ FrontEnd/react/router-v6-use/src/index.js
@@ -1,9 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+// import './1.基本路由'
+import './2.获取路径参数'
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
-
+/**
+ * 基本路由
+ * 路径参数
+ * Link NavLink导航 js导航调整
+ * 嵌套路由
+ * 获取参数 路径参数 location query 动态参数
+ * 路由保护
+ * 完整案例
+ */
