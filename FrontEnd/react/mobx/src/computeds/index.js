008241f3b929902a166c0b0f4780ff785754de09
wangtongmeng <tongmengwang@163.com>
2022-02-23 19:25:26 +0800

    react:mobx

--- FrontEnd/react/mobx/src/computeds/index.js
+++ FrontEnd/react/mobx/src/computeds/index.js
@@ -1,3 +1,4 @@
+// 如果computed的值没有在reactions中用到，那么每次都会计算
 import { makeObservable, observable, computed, autorun } from "mobx"
 
 class OrderLine {
@@ -43,3 +44,14 @@ stop()
 
 order.price = 3
 // 计算值和 autorun 都不会被重新计算.
+
+
+
+
+// OrderLine 拥有一个计算属性 `total`.
+const line = new OrderLine(2.0)
+
+// 如果你在 reaction 之外访问 `line.total`, 那么它每次都会被重新计算.
+setInterval(() => {
+    console.log(line.total)
+}, 1000)
