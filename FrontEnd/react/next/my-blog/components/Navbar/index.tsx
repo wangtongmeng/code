10a251582f269410d722a092f988e7ad91ad853b
wangtongmeng <tongmengwang@163.com>
2022-05-14 13:07:04 +0800

    '实现navbar'

--- FrontEnd/react/next/my-blog/components/Navbar/index.tsx
+++ FrontEnd/react/next/my-blog/components/Navbar/index.tsx
@@ -1,9 +1,25 @@
 import type { NextPage } from 'next'
+import Link from 'next/link';
+
+import styles from './index.module.scss';
+import { navs } from './config'
+import { useRouter } from 'next/router';
 
 const Navbar: NextPage = () => {
+  const { pathname } = useRouter()
+
   return (
-    <div>
-      导航
+    <div className={styles.navbar}>
+      <section className={styles.logoArea}>blog-c</section>
+      <section className={styles.linkArea}>
+        {
+          navs?.map(nav => (
+            <Link key={nav?.label} href={nav?.value}>
+              <a className={pathname === nav?.value ? styles.active : ''}>{nav?.label}</a>
+            </Link>
+          ))
+        }
+      </section>
     </div>
   )
 }
