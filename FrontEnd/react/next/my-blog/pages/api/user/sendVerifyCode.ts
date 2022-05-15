155135b74baf29b46a6687f5508a08d38f239979
wangtongmeng <tongmengwang@163.com>
2022-05-15 23:52:47 +0800

    mock短信验证码

--- FrontEnd/react/next/my-blog/pages/api/user/sendVerifyCode.ts
+++ FrontEnd/react/next/my-blog/pages/api/user/sendVerifyCode.ts
@@ -10,51 +10,65 @@ import { ironOptions } from 'config/index';
 export default withIronSessionApiRoute(sendVerifyCode, ironOptions);
 
 async function sendVerifyCode(req: NextApiRequest, res: NextApiResponse) {
-  const session: ISession = req.session;
-  const { to = '', templateId = '1' } = req.body;
-  const AppId = '8aaf07087d7fb5f6017d950ce83f04e1';
-  const AccountId = '8aaf07087d7fb5f6017d950ce72c04da';
-  const AuthToken = '91725ff244364cda9f1e1ea7d471e124';
-  const NowDate = format(new Date(), 'yyyyMMddHHmmss');
-  const SigParameter = md5(`${AccountId}${AuthToken}${NowDate}`);
-  const Authorization = encode(`${AccountId}:${NowDate}`);
+  // get user from database then:
   const verifyCode = Math.floor(Math.random() * (9999 - 1000)) + 1000;
-  const expireMinute = '5';
-  const url = `https://app.cloopen.com:8883/2013-12-26/Accounts/${AccountId}/SMS/TemplateSMS?sig=${SigParameter}`;
-
-  const response = await request.post(
-    url,
-    {
-      to,
-      templateId,
-      appId: AppId,
-      datas: [verifyCode, expireMinute],
-    },
-    {
-      headers: {
-        Authorization,
-      },
-    }
-  );
-
-  console.log(verifyCode)
-  console.log(response);
-  const { statusCode, templateSMS, statusMsg } = response as any;
-
-  if (statusCode === '000000') {
-    session.verifyCode = verifyCode;
-    await session.save();
-    res.status(200).json({
+  req.session.verifyCode = verifyCode;
+  await req.session.save();
+      res.status(200).json({
       code: 0,
-      msg: statusMsg,
+      msg: '000000',
       data: {
-        templateSMS
+        templateSMS: 'xxxx'
       }
     });
-  } else {
-    res.status(200).json({
-      code: statusCode,
-      msg: statusMsg
-    });
-  }
 }
+
+// async function sendVerifyCode(req: NextApiRequest, res: NextApiResponse) {
+//   const session: ISession = req.session;
+//   const { to = '', templateId = '1' } = req.body;
+//   const AppId = '8aaf07087d7fb5f6017d950ce83f04e1';
+//   const AccountId = '8aaf07087d7fb5f6017d950ce72c04da';
+//   const AuthToken = '91725ff244364cda9f1e1ea7d471e124';
+//   const NowDate = format(new Date(), 'yyyyMMddHHmmss');
+//   const SigParameter = md5(`${AccountId}${AuthToken}${NowDate}`);
+//   const Authorization = encode(`${AccountId}:${NowDate}`);
+//   const verifyCode = Math.floor(Math.random() * (9999 - 1000)) + 1000;
+//   const expireMinute = '5';
+//   const url = `https://app.cloopen.com:8883/2013-12-26/Accounts/${AccountId}/SMS/TemplateSMS?sig=${SigParameter}`;
+
+//   const response = await request.post(
+//     url,
+//     {
+//       to,
+//       templateId,
+//       appId: AppId,
+//       datas: [verifyCode, expireMinute],
+//     },
+//     {
+//       headers: {
+//         Authorization,
+//       },
+//     }
+//   );
+
+//   console.log(verifyCode)
+//   console.log(response);
+//   const { statusCode, templateSMS, statusMsg } = response as any;
+
+//   if (statusCode === '000000') {
+//     session.verifyCode = verifyCode;
+//     await session.save();
+//     res.status(200).json({
+//       code: 0,
+//       msg: statusMsg,
+//       data: {
+//         templateSMS
+//       }
+//     });
+//   } else {
+//     res.status(200).json({
+//       code: statusCode,
+//       msg: statusMsg
+//     });
+//   }
+// }
