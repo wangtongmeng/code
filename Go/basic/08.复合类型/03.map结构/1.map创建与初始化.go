d356e5b0710c5598bbea4950f799d86b7b36c77b
wangtongmeng <tongmengwang@163.com>
2022-08-12 09:09:29 +0800

    'go:basic'

--- Go/basic/08.复合类型/03.map结构/1.map创建与初始化.go
+++ Go/basic/08.复合类型/03.map结构/1.map创建与初始化.go
@@ -2,6 +2,10 @@ package main
 
 import "fmt"
 
+/**
+map是无序的，key唯一
+ */
+
 func main() {
 	var m1 map[int]string = map[int]string{1: "张三", 2: "李四", 3: "王五", 4: "itcast"} // key是唯一的
 	m2 := map[int]string{1: "张三", 2: "李四", 3: "王五", 4: "itcast"}
@@ -12,4 +16,10 @@ func main() {
 	fmt.Println(m1)          // map[1:张三 2:李四 3:王五 4:itcast]
 	fmt.Println(m2)          // map[1:张三 2:李四 3:王五 4:itcast]
 	fmt.Println(m3, len(m3)) // map[张三:16 李四:15] 2 len()返回的是map中已有的键值对个数。
+
+	// 创建空map
+	var m11 = map[int]string{}
+	m22 := map[int]string{}
+	m33 := make(map[string]int, 10)
+	fmt.Println(m11, m22,m33) // map[] map[] map[]
 }
