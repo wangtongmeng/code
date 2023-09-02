8413c697dd344fcd03f1193795dd232d2e945a99
wangtongmeng <tongmengwang@163.com>
2023-09-02 17:44:35 +0800

    update

---
+++ fe/vue/vue3/pinia/pinia-resource/src/stores/counter.js
@@ -0,0 +1,15 @@
+import { defineStore } from "pinia";
+// id 必须是唯一的，否则会覆盖
+export const useCounterStore = defineStore("counter", {
+  state: () => ({ count: 0 }),
+  getters: {
+    double() {
+      return this.count * 2;
+    },
+  },
+  actions: {
+    increment() {
+      this.count++;
+    },
+  },
+});
