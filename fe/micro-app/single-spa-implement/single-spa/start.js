1dd07993fb4393ce336dd6369f5b7c77d3be28ac
wangtongmeng <tongmengwang@163.com>
2023-09-09 16:32:34 +0800

    single-spa: 应用启动到卸载

--- fe/micro-app/single-spa-implement/single-spa/start.js
+++ fe/micro-app/single-spa-implement/single-spa/start.js
@@ -1,3 +1,7 @@
-export function start() {
-   
+import { reroute } from "./navigation/reroute.js";
+
+export let started = false; // 默认没有调用start方法
+export function start(){
+    started = true; // 用户启动了
+    reroute()
 }
