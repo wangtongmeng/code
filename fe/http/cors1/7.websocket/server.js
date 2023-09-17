c2031ba40004ccf90bcf1dc9c971d5eb0b60c48f
wangtongmeng <tongmengwang@163.com>
2023-09-17 08:53:21 +0800

    update

---
+++ fe/http/cors1/7.websocket/server.js
@@ -0,0 +1,11 @@
+let express = require('express')
+let app = express()
+
+let WebSocket = require('ws')
+let wss = new WebSocket.Server({port: 3000})
+wss.on('connection', function(ws) { 
+  ws.on('message', function (data) {
+    console.log(data);
+    ws.send('data from server')
+  })
+})
