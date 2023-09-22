e57d088f8d7d85089b477a7639f1cce979178f3e
wangtongmeng <tongmengwang@163.com>
2023-09-22 17:06:56 +0800

    update

--- fe/alg-js/sort/quickSort.js
+++ fe/alg-js/sort/quickSort.js
@@ -1,4 +1,4 @@
-// 归并排序
+// 快速排序
 
 Array.prototype.quickSort = function () {
   const rec = (arr) => {
@@ -29,3 +29,61 @@ console.log(arr); // [ 1, 2, 3, 4, 5 ]
 //   递归的时间复杂度是O(logN)
 //   分区操作的时间复杂度是O(n)
 //   时间复杂度：O(n * logN)
+
+
+function quickSort(arr) {
+  if (arr.length < 2) {
+    return arr
+  }
+
+  let flag = arr[0]
+  let left = []
+  let right = []
+  // 注意这里从1开始，因为第一个拿去做标识位了
+  for (let i = 1; i < arr.length; i++) {
+    if (arr[i] > flag) {
+      right.push(arr[i])
+    } else {
+      left.push(arr[i])
+    }
+  }
+  return quickSort(left).concat(flag, quickSort(right))
+}
+
+console.log(quickSort(arr)); //  [ 1, 2, 3, 4, 5 ]
+// 时间复杂度：O(n * logN)
+// 空间复杂度 O(n * logN)
+
+// 原地快排
+function quickSort2(arr, start, end) {
+  function quick1(arr, start, end) {
+    let init = start
+    let flag = arr[init]
+    start++
+    while(start <= end) {
+      while (arr[end] > flag) {
+        end--
+      }
+      while(arr[start] < flag) {
+        start++
+      }
+      if (start < end) {
+        [arr[start], arr[end]] = [arr[end], arr[start]]
+        start++
+        end--
+      }
+    }
+    [arr[init], arr[start -1]] = [arr[start -1], arr[init]]
+    return start
+  }
+  // 通过双指针找到左边比flag大的，右边比flag小的进行交换
+  if (start < end) {
+    let index = quick1(arr, start, end) // 标志位的值
+    quickSort2(arr, start, index - 1)
+    quickSort2(arr, index, end)
+  }
+  return arr
+}
+console.log(quickSort2(arr, 0, arr.length -1)); // [ 1, 2, 3, 4, 5 ]
+// 时间复杂度：O(n * logN)
+// 空间复杂度 O(1)
