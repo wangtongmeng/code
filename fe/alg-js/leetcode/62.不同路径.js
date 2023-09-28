dca74840161c11613d3fded1a586520f4e8cc203
wangtongmeng <tongmengwang@163.com>
2023-09-28 21:59:30 +0800

    update

--- fe/alg-js/leetcode/62.不同路径.js
+++ fe/alg-js/leetcode/62.不同路径.js
@@ -11,7 +11,14 @@
  * @return {number}
  */
 var uniquePaths = function(m, n) {
-
+  let dp = new Array(m).fill(1).map(() => new Array(n).fill(1))
+   // dp[i][j] 表示到达（i，j） 点的路径数
+  for (let i = 1; i < m; i++) {
+    for (let j = 1; j < n; j++) {
+      dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+    }
+  }
+  return dp[m-1][n-1]
 };
 // @lc code=end
 
