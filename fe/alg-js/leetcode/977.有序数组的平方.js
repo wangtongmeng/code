cdf716d49addc1224ae850b578d08a97178ecbd4
wangtongmeng <tongmengwang@163.com>
2023-10-02 07:14:05 +0800

    update

---
+++ fe/alg-js/leetcode/977.有序数组的平方.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=977 lang=javascript
+ *
+ * [977] 有序数组的平方
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var sortedSquares = function(nums) {
+  // 双指针：数组两边的数字平方最大，越往里越小，从两边取出平方较大的从数组尾部往前放
+  let n = nums.length
+  let ret = new Array(n).fill(0)
+  let i = 0, j = n - 1, k = n - 1
+  while(i <= j) {
+    let left = nums[i] * nums[i]
+    let right = nums[j] * nums[j]
+    if (left < right) {
+      ret[k--] = right
+      j--
+    } else {
+      ret [k--] = left
+      i++
+    }
+  } 
+  return ret
+};
+// @lc code=end
+
