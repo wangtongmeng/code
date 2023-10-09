8c23dbfb0ac01cdb6c0b1ef62299936da44cac6d
wangtongmeng <tongmengwang@163.com>
2023-10-10 07:44:58 +0800

    update

---
+++ fe/alg-js/leetcode/349.两个数组的交集.js
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  if (nums1.length === 0 || nums2.length === 0) {
+    return []
+  }
+  let map = new Map()
+  let ret = []
+  
+  for (i = 0; i < nums1.length; i++) {
+    let n = nums1[i]
+    if (!map.get(n)) {
+      map.set(n, 1)
+    }
+  }
+  for (i = 0; i < nums2.length; i++) {
+    let n = nums2[i]
+    if (map.get(n)) {
+      ret.push(n)
+      map.set(n, 0)
+    }
+  }
+
+  return ret
+};
+// @lc code=end
+
