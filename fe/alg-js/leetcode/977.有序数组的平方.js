b44b65e12fac95d37ea4bad936eccffb13e2a63d
wangtongmeng <tongmengwang@163.com>
2023-10-20 23:19:01 +0800

    update

--- fe/alg-js/leetcode/977.有序数组的平方.js
+++ fe/alg-js/leetcode/977.有序数组的平方.js
@@ -10,22 +10,41 @@
  * @return {number[]}
  */
 var sortedSquares = function(nums) {
-  // 双指针：数组两边的数字平方最大，越往里越小，从两边取出平方较大的从数组尾部往前放
-  let n = nums.length
-  let ret = new Array(n).fill(0)
-  let i = 0, j = n - 1, k = n - 1
-  while(i <= j) {
-    let left = nums[i] * nums[i]
-    let right = nums[j] * nums[j]
-    if (left < right) {
-      ret[k--] = right
-      j--
+  let len = nums.length
+  let ret = new Array(len).fill(0)
+  let left = 0;
+  let right = len - 1
+  let cur = len - 1
+  while (left <= right) {
+    let leftSquare = nums[left] * nums[left]
+    let rightSquare = nums[right] * nums[right]
+    if (leftSquare < rightSquare) {
+      ret[cur] = rightSquare
+      right--
     } else {
-      ret [k--] = left
-      i++
+      ret[cur] = leftSquare
+      left++
     }
-  } 
+    cur--
+  }
   return ret
 };
 // @lc code=end
+
+// 双指针：数组两边的数字平方最大，越往里越小，从两边取出平方较大的从数组尾部往前放
+// let n = nums.length
+// let ret = new Array(n).fill(0)
+// let i = 0, j = n - 1, k = n - 1
+// while(i <= j) {
+//   let left = nums[i] * nums[i]
+//   let right = nums[j] * nums[j]
+//   if (left < right) {
+//     ret[k--] = right
+//     j--
+//   } else {
+//     ret [k--] = left
+//     i++
+//   }
+// } 
+// return ret
 
