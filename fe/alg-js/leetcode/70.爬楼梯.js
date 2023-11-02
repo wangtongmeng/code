8650935bdc8fa0bfab6fc5e7061e2e5be2ea444a
wangtongmeng <tongmengwang@163.com>
2023-11-02 08:04:08 +0800

    update

--- fe/alg-js/leetcode/70.爬楼梯.js
+++ fe/alg-js/leetcode/70.爬楼梯.js
@@ -10,24 +10,34 @@
  * @return {number}
  */
 var climbStairs = function(n) {
-  // 动态规划 斐波那契
+  if (n <= 2) {
+    return n
+  }
+ let dp = [1, 2]
+ for (let i = 2; i <= n; i++) {
+  dp[i] = dp[i - 1] + dp[i - 2]
+ }
+ return dp[n - 1]
+};
+// @lc code=end
+
+
+ // 动态规划 斐波那契
   // let dp = [1,2]
   // for (let i = 2; i <= n; i++) {
   //   dp[i] = dp[i -1] + dp[i - 2]
   // }
   // return dp[n - 1]
   // 优化空间复杂度
-  if (n <= 2) {
-    return n
-  }
-  let n1 = 1
-  let n2 = 2
-  for (let i = 3; i <= n; i++) {
-    let temp = n1 + n2
-    n1 = n2
-    n2 = temp
-  }
-  return n2
-};
-// @lc code=end
+  // if (n <= 2) {
+  //   return n
+  // }
+  // let n1 = 1
+  // let n2 = 2
+  // for (let i = 3; i <= n; i++) {
+  //   let temp = n1 + n2
+  //   n1 = n2
+  //   n2 = temp
+  // }
+  // return n2
 
