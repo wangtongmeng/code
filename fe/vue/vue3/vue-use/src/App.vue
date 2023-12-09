fb06530e4951af9525c0912da1e5021e0c6f49b1
wangtongmeng <tongmengwang@163.com>
2023-12-09 08:54:41 +0800

    vue:use

---
+++ fe/vue/vue3/vue-use/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import {ref} from 'vue'
+const msg = ref('你好')
+const rawHtml = ref('<span style="color: red">This should be red.</span>')
+</script>
+
+// 文本插值
+// <template>
+//   <span>{{msg}}</span>
+// </template>
+
+// 原始Html
+<template>
+  <div>
+  <p>{{rawHtml}}</p>
+  <p v-html="rawHtml"></p>
+  </div>
+  
+</template>
+  
+// <RouterView />
+
+<style scoped>
+
+</style>
