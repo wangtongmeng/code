74c26c36dbcd80392dbf28d505a70ba6ac0552f5
wangtongmeng <tongmengwang@163.com>
2024-01-11 07:00:13 +0800

    solutions: vueuse 全屏

---
+++ fe/solutions/全屏与退出全屏/vue-use-全屏退出全屏/src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div ref="el" class="box">
+    <button @click="toggle" class="cursor">
+      {{ !isFullscreen ? '全屏模式' : '退出全屏' }}
+    </button>
+    <br />
+    <button @click="enter">进入全屏-enter</button>
+    <br />
+    <button @click="exit">退出全屏-exit</button>
+  </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import { useFullscreen } from '@vueuse/core';
+
+const el = ref();
+const { isFullscreen, toggle, enter, exit } = useFullscreen(el);
+
+</script>
+
+<style  scoped>
+.box {
+  background-color: aqua;
+  width: 500px;
+  height: 300px;
+}
+</style>
