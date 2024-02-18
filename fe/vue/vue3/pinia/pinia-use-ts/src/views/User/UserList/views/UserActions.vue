251045251c662396baae729643fb1f304d3a0773
wangtongmeng <tongmengwang@163.com>
2024-02-18 17:07:23 +0800

    vue: pinia

---
+++ fe/vue/vue3/pinia/pinia-use-ts/src/views/User/UserList/views/UserActions.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    用户标题+操作按钮
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ fe/vue/vue3/pinia/pinia-use-ts/src/views/User/UserList/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <Layout>
+    <UserActions />
+    <UserTable />
+  </Layout>
+</template>
+
+<script setup lang="ts">
+import Layout from '../components/Layout.vue';
+import UserActions from './UserActions.vue';
+import UserTable from './UserTable.vue';
+</script>
+
+<style scoped>
+
+</style>
