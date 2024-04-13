bab817b77bcca1e4b0beac189bdab20d76b5d533
wangtongmeng <tongmengwang@163.com>
2024-04-14 06:58:24 +0800

    up

---
+++ python/base/2.基础/4.条件判断.py
@@ -0,0 +1,42 @@
+
+
+
+# if 
+# age = 20
+# if age >= 18:
+#   print('age is', age)
+#   print('adult')
+
+# if else
+# age = 3
+# if age >= 18:
+#     print('your age is', age)
+#     print('adult')
+# else:
+#     print('your age is', age)
+#     print('teenager')
+
+
+# if elif else
+# age = 3
+# if age >= 18:
+#     print('adult')
+# elif age >= 6:
+#     print('teenager')
+# else:
+#     print('kid')
+
+# if 简写
+# 只要x是非零数值、非空字符串、非空list等，就判断为True，否则为False
+# x = 1
+# if x:
+#     print('True')
+
+
+# input 结合 if 判断
+s = input('birth: ')
+birth = int(s)
+if birth < 2000:
+  print('00前')
+else:
+  print('00后')
---
+++ python/base/2.基础/5.模式匹配.py
@@ -0,0 +1,29 @@
+# 模式匹配
+
+# match 类似 js 中的 switch
+# score = 'B'
+# match score:
+#   case 'A':
+#     print('score is A')
+#   case 'B':
+#     print('score is B')
+#   case 'C':
+#     print('score is C')
+#   case '_': # _表示匹配到其他任何情况
+#     print('score is ???.')
+
+
+# 复杂匹配
+age = 16
+
+match age:
+    case x if x < 10:
+        print(f'< 10 years old: {x}')
+    case 10:
+        print('10 years old.')
+    case 11 | 12 | 13 | 14 | 15 | 16 | 17 | 18:
+        print('11~18 years old.')
+    case 19:
+        print('19 years old.')
+    case _:
+        print('not sure.')
