c870415d395b8b13b5430ef60fe3eba43f3c700b
wangtongmeng <tongmengwang@163.com>
2024-08-01 23:25:52 +0800

    up

--- fe/interview/javascript/手写题/原型与原型链/instanceof.js
+++ fe/interview/javascript/手写题/原型与原型链/instanceof.js
@@ -1,17 +1,12 @@
 function myInstanceOf(left, right) {
-  // 获取对象的原型
   let proto = Object.getPrototypeOf(left);
-  // 获取构造函数的 prototype
   let prototype = right.prototype;
-
-  // 判断构造函数的 prototype 对象是否在对象的原型链上
-  while(true) {
+  while (true) {
     if (!proto) return false;
     if (proto === prototype) return true;
-    proto = Object.getPrototypeOf(proto)
+    proto = Object.getPrototypeOf(proto);
   }
 }
 
-
-console.log(myInstanceOf(function fn1(){}, Object)); // true
-console.log(myInstanceOf(function fn1(){}, Array)); // false
+console.log(myInstanceOf(function fn1() {}, Object)); // true
+console.log(myInstanceOf(function fn1() {}, Array)); // false
