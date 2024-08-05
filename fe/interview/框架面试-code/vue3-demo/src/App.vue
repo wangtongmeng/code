832886fc6a0f8777088c4dedb00d0d5d0d1ce6ba
wangtongmeng <tongmengwang@163.com>
2024-08-05 22:37:32 +0800

    up

--- fe/interview/框架面试-code/vue3-demo/src/App.vue
+++ fe/interview/框架面试-code/vue3-demo/src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <!-- <HelloWorld msg="Hello Vue 3.0 + Vite" @onSayHello="sayHello"/> -->
+  <HelloWorld msg="Hello Vue 3.0 + Vite" @onSayHello="sayHello" />
   <!-- <life-cycles :msg="msg" v-if="flag"/> -->
   <!-- <Ref/> -->
   <!-- <ToRef/> -->
@@ -10,29 +10,29 @@
   <!-- <MousePosition v-if="flag"/> -->
   <!-- <Watch/> -->
   <!-- <GetInstance/> -->
-  <ToRefsForDeepObj/>
+  <!-- <ToRefsForDeepObj/> -->
 
-  <hr>
+  <hr />
   <button @click="changeHandler">change msg</button>
   <button @click="changeFlagHandler">change flag</button>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import LifeCycles from './components/LifeCycles.vue'
-import Ref from './components/Ref.vue'
-import ToRef from './components/ToRef.vue'
-import ToRefs from './components/ToRefs.vue'
-import RefTemplate from './components/RefTemplate.vue'
-import WhyRef from './components/WhyRef.vue'
-import VModel from './components/VModel/index.vue'
-import MousePosition from './components/MousePosition/index.vue'
-import Watch from './components/Watch.vue'
-import GetInstance from './components/GetInstance.vue'
-import ToRefsForDeepObj from './components/ToRefsForDeepObj.vue'
+import HelloWorld from "./components/HelloWorld.vue";
+import LifeCycles from "./components/LifeCycles.vue";
+import Ref from "./components/Ref.vue";
+import ToRef from "./components/ToRef.vue";
+import ToRefs from "./components/ToRefs.vue";
+import RefTemplate from "./components/RefTemplate.vue";
+import WhyRef from "./components/WhyRef.vue";
+import VModel from "./components/VModel/index.vue";
+import MousePosition from "./components/MousePosition/index.vue";
+import Watch from "./components/Watch.vue";
+import GetInstance from "./components/GetInstance.vue";
+import ToRefsForDeepObj from "./components/ToRefsForDeepObj.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
     HelloWorld,
     LifeCycles,
@@ -45,25 +45,25 @@ export default {
     MousePosition,
     Watch,
     GetInstance,
-    ToRefsForDeepObj
+    ToRefsForDeepObj,
   },
 
   data() {
     return {
-      msg: 'hello vue3',
-      flag: true
-    } 
+      msg: "hello vue3",
+      flag: true,
+    };
   },
   methods: {
     changeHandler() {
-      this.msg = 'hello vue3' + Date.now()
+      this.msg = "hello vue3" + Date.now();
     },
     changeFlagHandler() {
-      this.flag = !this.flag
+      this.flag = !this.flag;
     },
     sayHello(info) {
-      console.log('hello', info)
-    }
-  }
-}
+      console.log("hello", info);
+    },
+  },
+};
 </script>
