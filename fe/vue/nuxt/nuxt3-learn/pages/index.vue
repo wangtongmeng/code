56364a1ba52011472356c2c0835dabf3f3b7b602
wangtongmeng <tongmengwang@163.com>
2024-09-01 10:31:01 +0800

    up

--- fe/vue/nuxt/nuxt3-learn/pages/index.vue
+++ fe/vue/nuxt/nuxt3-learn/pages/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="flex items-center flex-col gap-2 py-4">
-    <!-- <h1>Index Page</h1>
+    <h1>Index Page</h1>
     <div>
-      <NButton>hello</NButton>
+      <UButton>Button</UButton>
     </div>
-    <NuxtLink to="/detail/1">detail 1</NuxtLink> -->
+    <NuxtLink to="/detail/1">detail 1</NuxtLink>
     <div v-for="post in posts" :key="post.id">
       <NuxtLink class="text-lg" :to="`/detail/${post.id}`">{{
         post.title
