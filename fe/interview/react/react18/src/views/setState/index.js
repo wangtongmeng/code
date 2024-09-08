081b74e2784113df3f4e1a9432adb34f4f1f8c5c
王童孟 <tongmengwang@163.com>
2024-09-08 09:47:15 +0800

    up

--- fe/interview/react/react18/src/views/setState/index.js
+++ fe/interview/react/react18/src/views/setState/index.js
@@ -2,74 +2,30 @@ import React from "react";
 class Counter extends React.Component {
   state = { number: 0 };
   buttonClick = () => {
-    // 第一题 setState是异步的，并且结果合并了
-    // this.setState({ number: this.state.number + 1 }, () => {
-    //   console.log("回调", this.state.number); // 1
-    // });
-    // console.log(this.state.number); // 0
-    // this.setState({ number: this.state.number + 1 }, () => {
-    //   console.log("回调", this.state.number); // 1
-    // });
-    // console.log(this.state.number); // 0
+    // 第1题
+    // this.setState({ number: this.state.number + 1 });
+    // console.log(this.state.number);
+    // this.setState({ number: this.state.number + 1 });
+    // console.log(this.state.number);
+    // setTimeout(() => {
+    //   this.setState({ number: this.state.number + 1 });
+    //   console.log(this.state.number);
+    //   this.setState({ number: this.state.number + 1 });
+    //   console.log(this.state.number);
+    // }, 0);
     // 0 0 1 1
-
-    // 第二题 通过函数的形式，让每次state相加都生效
-    // this.setState(
-    //   (state) => ({ number: state.number + 1 }),
-    //   () => {
-    //     console.log(this.state.number);
-    //   }
-    // );
-    // console.log(this.state.number);
-    // this.setState(
-    //   (state) => ({ number: state.number + 1 }),
-    //   () => {
-    //     console.log(this.state.number);
-    //   }
-    // );
-    // console.log(this.state.number);
-    // 0 0 2 2
-
+    // 第2题 如何拿到最新的值
+    // this.setState({ number: this.state.number + 1 }, (x) => {
+    //   console.log(this.state.number); // 1
+    // });
     // 第3题
-    this.setState(
-      (state) => ({ number: state.number + 1 }),
-      () => {
-        console.log(this.state.number);
-      }
-    );
-    console.log("1", this.state.number);
-    this.setState(
-      (state) => ({ number: state.number + 1 }),
-      () => {
-        console.log(this.state.number);
-      }
-    );
-    console.log("2", this.state.number);
-
-    setTimeout(() => {
-      this.setState(
-        (state) => ({ number: state.number + 1 }),
-        () => {
-          console.log(this.state.number);
-        }
-      );
-      console.log("3", this.state.number);
-
-      this.setState(
-        (state) => ({ number: state.number + 1 }),
-        () => {
-          console.log(this.state.number);
-        }
-      );
-      console.log("4", this.state.number);
-    });
-    //  1 0
-    //  2 0
-    //  2
-    //  2
-    //  3 2
-    //  4 2
-    //  4
+    // this.setState({ number: 1 }, () => {
+    //   console.log(this.state.number); // 2
+    // });
+    // this.setState({ number: 2 }, () => {
+    //   console.log(this.state.number); // 2
+    // });
+    // 说明回调是统一执行的
   };
   render() {
     return (
