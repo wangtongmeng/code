b760694961204c1cde2bf3e7bf2c9bd927913a16
王童孟 <tongmengwang@163.com>
2024-09-08 11:16:17 +0800

    up

---
+++ fe/node/nest/nest-use/src/person/person.controller.ts
@@ -0,0 +1,39 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete, Query } from '@nestjs/common';
+import { PersonService } from './person.service';
+import { CreatePersonDto } from './dto/create-person.dto';
+import { UpdatePersonDto } from './dto/update-person.dto';
+
+@Controller('api/person')
+export class PersonController {
+  constructor(private readonly personService: PersonService) {}
+
+  @Get('find')
+  query(@Query('name') name: string, @Query('age') age:number) {
+    return `name=${name}, age= ${age}`
+  }
+
+  @Post()
+  create(@Body() createPersonDto: CreatePersonDto) {
+    return this.personService.create(createPersonDto);
+  }
+
+  @Get()
+  findAll() {
+    return this.personService.findAll();
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return this.personService.findOne(+id);
+  }
+
+  @Patch(':id')
+  update(@Param('id') id: string, @Body() updatePersonDto: UpdatePersonDto) {
+    return this.personService.update(+id, updatePersonDto);
+  }
+
+  @Delete(':id')
+  remove(@Param('id') id: string) {
+    return this.personService.remove(+id);
+  }
+}
