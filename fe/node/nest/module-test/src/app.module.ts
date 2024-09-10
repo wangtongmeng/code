9e26c8bac0453597f22b792ae26b333650266afd
王童孟 <tongmengwang@163.com>
2024-09-10 17:33:09 +0800

    up

---
+++ fe/node/nest/module-test/src/app.module.ts
@@ -0,0 +1,14 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { AaaModule } from './aaa/aaa.module';
+import { BbbModule } from './bbb/bbb.module';
+import { CccService } from './ccc.service';
+import { DddService } from './ddd.service';
+
+@Module({
+  imports: [AaaModule, BbbModule],
+  controllers: [AppController],
+  providers: [AppService, CccService, DddService],
+})
+export class AppModule {}
