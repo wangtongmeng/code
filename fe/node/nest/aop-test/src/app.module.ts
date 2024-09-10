d0d1ce45c337f9985e2d32b5ba4806ece8a0df2a
王童孟 <tongmengwang@163.com>
2024-09-10 09:46:41 +0800

    up

---
+++ fe/node/nest/aop-test/src/app.module.ts
@@ -0,0 +1,23 @@
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { LogMiddleware } from './log.middleware';
+// import { APP_GUARD } from '@nestjs/core';
+// import { LoginGuard } from './login.guard';
+
+@Module({
+  imports: [],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    // {
+    //   provide: APP_GUARD,
+    //   useClass: LoginGuard,
+    // },
+  ],
+})
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(LogMiddleware).forRoutes('aaa*');
+  }
+}
---
+++ fe/node/nest/aop-test/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NextFunction } from 'express';
+import { LoginGuard } from './login.guard';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.use(function (req: Request, res: Response, next: NextFunction) {
+    console.log('before', req.url);
+    next();
+    console.log('after');
+  });
+
+  // app.useGlobalGuards(new LoginGuard());
+
+  await app.listen(3000);
+}
+bootstrap();
